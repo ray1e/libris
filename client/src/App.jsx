d1df3c1b922827508dd05a77292c56b943c4c056
collins kimani <qeshyy@gmail.com>
2026-09-21 14:37:59 +0300

    style(App): remove trailing whitespace

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { MobileLayout } from "./components/Layout.jsx";
-import { useState } from "react";
 import { Dashboard } from "./pages/Dashboard.jsx";
 import { Route, Routes } from "react-router-dom";
 import { AddBook } from "./pages/AddBook.jsx";
 import { BookView } from "./pages/BookView.jsx";
+
 function App() {
   //const [rating, setRating] = useState(0);
   return (
