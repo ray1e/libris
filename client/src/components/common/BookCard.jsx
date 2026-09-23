315d042c1d522418c227e165ae35a792e894928a
collins kimani <qeshyy@gmail.com>
2026-09-23 09:14:31 +0300

    style(BookCard): wrap import statements for better readability

--- client/src/components/common/BookCard.jsx
+++ client/src/components/common/BookCard.jsx
@@ -1,9 +1,16 @@
 import { BookInfo } from "./BookInfo.jsx";
 
-export function BookCard({ book, rating, onSelect, handleKeyDown, imageLink, bookTag }) {
+export function BookCard({
+  book,
+  rating,
+  onSelect,
+  handleKeyDown,
+  imageLink,
+  bookTag,
+}) {
   return (
     <article
-      className="max-w-40 max-h-76 sm:max-w-48 sm:h-84 md:max-52 md:h-88 bg-surface-default  rounded-lg outline-1 outline-offset-1  shadow-sm outline-border-default inline-flex flex-col justify-center items-start gap-2 pb-2"
+      className="w-full max-h-78 bg-surface-default  rounded-lg outline-1 outline-offset-1  shadow-sm outline-border-default inline-flex flex-col justify-center items-start"
       onClick={onSelect}
       onKeyDown={handleKeyDown}
       tabIndex={0}
@@ -11,17 +18,11 @@ export function BookCard({ book, rating, onSelect, handleKeyDown, imageLink, boo
       <img
         src={imageLink}
         alt="Book cover placeholder"
-        width={300}
-        height={400}
-        className="h-44 rounded-lg object-cover w-full aspect-2/3 shrink-0"
+
+        className="h-40 rounded-lg object-cover w-full aspect-2/3 shrink-0"
       />
-      <div className="flex-1 pl-2">
-        <BookInfo
-          rating={rating}
-          book={book}
-          tag={bookTag}
-          
-        />
+      <div className="flex-1 pl-2  py-2 mt-auto">
+        <BookInfo rating={rating} book={book} tag={bookTag} />
       </div>
     </article>
   );
