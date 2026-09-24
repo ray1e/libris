993f46c20759bad9ffd5d13c9abcd11c13eac311
collins kimani <qeshyy@gmail.com>
2026-09-24 11:34:55 +0300

    feat(util): add date formatting utility

--- client/src/utils/dateFormatter.js
+++ client/src/utils/dateFormatter.js
@@ -1,1 +1,12 @@
-const 
+const dateFormatter = new Intl.DateTimeFormat("en-GB", {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+  timeZone: "UTC",
+});
+
+export const formatDate = (dateValue) => {
+  if (!dateValue) return "N/A";
+  const date = new Date(dateValue);
+  return Number.isNaN(date.getTime()) ? "NAN" : dateFormatter.format(date);
+};
